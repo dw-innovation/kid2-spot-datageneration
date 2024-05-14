1ba7344aca5a5b2eae3f523e2680e140c443a149
Ipek Baris Schlicht <ipek.baris-schlicht@dw.com>
2024-05-14 13:24:15 +0200

    fixes #14

---
+++ datageneration/tests/test_tag_to_imr.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import unittest
+
+from datageneration.tags_to_imr import 
+
+'''
+Execute it as follows: python -m unittest datageneration.tests.test_tag_to_imr
+'''
+
+
+class TestTag2ImrConverter(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_property_generate(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
