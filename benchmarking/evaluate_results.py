148a8cfc71ace2a9000d436451e1ae496d6a9c9c
Ipek Baris Schlicht <ipek.baris-schlicht@dw.com>
2024-07-19 15:26:12 +0200

    add prop eval, change res data type

--- benchmarking/evaluate_results.py
+++ benchmarking/evaluate_results.py
@@ -1,22 +1,42 @@
 import copy
+import enum
 import pandas as pd
 import yaml
 from argparse import ArgumentParser
 from pydantic import BaseModel, Field
 from tqdm import tqdm
 
-from benchmarking.yaml_parser import validate_and_fix_yaml
 from benchmarking.utils import write_output
+from benchmarking.yaml_parser import validate_and_fix_yaml
+
+
+class ResultDataType(enum.Enum):
+    TRUE = 'TRUE'
+    FALSE = 'FALSE'
+    NOT_APPLICABLE = 'NOT_APPLICABLE'
+
 
 class Result(BaseModel, frozen=True):
     yaml_true_string: str = Field(...)
     yaml_pred_string: str = Field(...)
-    is_parsable_yaml: bool = Field(description="True if yaml can be parsed, otherwise False", default=False)
-    is_area_same: bool = Field(description="True if areas are equal, otherwise False", default=False)
-    are_entities_same: bool = Field(description="True if entity are equal, otherwise False", default=False)
-    are_relations_same: bool = Field(description="True if entity are equal, otherwise False", default=False)
+    is_parsable_yaml: ResultDataType = Field(description="True if yaml can be parsed, otherwise False",
+                                             default=ResultDataType.FALSE)
+    is_area_exact_match: ResultDataType = Field(description="True if areas are equal, otherwise False",
+                                                default=ResultDataType.FALSE)
+    num_entities_on_ref_data: int = 0
+    num_entities_on_gen_data: int = 0
+    num_relations_on_ref_data: int = 0
+    num_relations_on_ref_data: int = 0
 
-def is_parsable_yaml(yaml_string):
+    are_entities_exactly_same: ResultDataType = Field(description="True if entity are equal, otherwise False",
+                                                      default=ResultDataType.FALSE)
+    are_relations_exactly_same: ResultDataType = Field(description="True if entity are equal, otherwise False",
+                                                       default=ResultDataType.NOT_APPLICABLE)
+    are_properties_exactly_same: ResultDataType = Field(description="True if entity are equal, otherwise False",
+                                                        default=ResultDataType.NOT_APPLICABLE)
+
+
+def is_parsable_yaml(yaml_string) -> ResultDataType:
     """
     Checks whether the input batch of YAML strings is parsable.
 
@@ -25,9 +45,9 @@ def is_parsable_yaml(yaml_string):
     parsed_yaml = None
     try:
         parsed_yaml = yaml.safe_load(yaml_string)
-        is_parsable = True
+        is_parsable = ResultDataType.TRUE
     except Exception as e:
-        is_parsable = False
+        is_parsable = ResultDataType.FALSE
         # try to parse it by using the custom parser from the backend
         try:
             parsed_yaml = validate_and_fix_yaml(yaml_string)
@@ -36,7 +56,7 @@ def is_parsable_yaml(yaml_string):
     return is_parsable, parsed_yaml
 
 
-def compare_areas(area1, area2):
+def compare_areas(area1, area2) -> ResultDataType:
     """
     Checks if two areas are identical.
 
@@ -44,10 +64,10 @@ def compare_areas(area1, area2):
     :param area2: The second area to compare.
     :return: Boolean whether the two areas are the same.
     """
-    return area1 == area2
+    return ResultDataType.TRUE if (area1 == area2) else ResultDataType.FALSE
 
 
-def compare_properties(props1, props2):
+def compare_properties(props1, props2) -> ResultDataType:
     """
     Check if two lists of properties are identical. The lists are first sorted via their names, to make sure the order
     does not affect the results.
@@ -60,10 +80,10 @@ def compare_properties(props1, props2):
         return False
     props1_sorted = sorted(props1, key=lambda x: x['name'])
     props2_sorted = sorted(props2, key=lambda x: x['name'])
-    return props1_sorted == props2_sorted
+    return ResultDataType.TRUE if (props1_sorted == props2_sorted) else ResultDataType.FALSE
 
 
-def compare_entities(entities1, entities2):
+def compare_entities(entities1, entities2) -> ResultDataType:
     """
     Check if two lists of entities are identical. The lists are first sorted via their names, to make sure the order
     does not affect the results.
@@ -73,20 +93,20 @@ def compare_entities(entities1, entities2):
     :return: Boolean whether the two entity lists are the same.
     """
     if len(entities1) != len(entities2):
-        return False
+        return ResultDataType.FALSE
     entities1_sorted = sorted(entities1, key=lambda x: x['name'])
     entities2_sorted = sorted(entities2, key=lambda x: x['name'])
     for ent1, ent2 in zip(entities1_sorted, entities2_sorted):
         if 'type' not in ent1:
-            return False
+            return ResultDataType.FALSE
         if ent1['name'] != ent2['name'] or ent1['type'] != ent2['type']:
-            return False
+            return ResultDataType.FALSE
         if not compare_properties(ent1.get('properties', []), ent2.get('properties', [])):
-            return False
-    return True
+            return ResultDataType.FALSE
+    return ResultDataType.TRUE
 
 
-def prepare_relation(data):
+def prepare_relation(data) -> ResultDataType:
     """
     In order to compare relations independent of the order of entities, it is not sufficient to have numeric
     references for target and source. This method therefore replaces the numeric pointers with the descriptors (names)
@@ -105,7 +125,7 @@ def prepare_relation(data):
     return prepped_relation
 
 
-def compare_relations(relations1, relations2):
+def compare_relations(relations1, relations2) -> ResultDataType:
     """
     Check if two lists of relations are identical. There are two different ways how the comparison is done, based on
     whether the order of source and target is relevant or not (only the case in "contains" relations).
@@ -117,7 +137,7 @@ def compare_relations(relations1, relations2):
     :return: Boolean whether the two relations lists are the same.
     """
     if len(relations1) != len(relations2):
-        return False
+        return ResultDataType.FALSE
     r1 = set()
     r2 = set()
     c1 = list()
@@ -134,11 +154,11 @@ def compare_relations(relations1, relations2):
     c1s = sorted(c1, key=lambda x: (x[0], x[1]))
     c2s = sorted(c2, key=lambda x: (x[0], x[1]))
     if c1s != c2s:
-        return False
+        return ResultDataType.FALSE
     if r1 != r2:
-        return False
+        return ResultDataType.TRUE
 
-    return True
+    return ResultDataType.TRUE
 
 
 def compare_yaml(yaml_true_string, yaml_pred_string) -> Result:
@@ -150,36 +170,53 @@ def compare_yaml(yaml_true_string, yaml_pred_string) -> Result:
     :param yaml_pred_string: The first YAML to compare.
     :return: Boolean whether the two YAMLs are the same.
     """
-    _, data1 = is_parsable_yaml(yaml_true_string)
-    _is_parsable_yaml, data2 = is_parsable_yaml(yaml_pred_string)
-    _is_area_same = False
-    are_entities_same = False
-    are_relations_same = False
+    _, ref_data = is_parsable_yaml(yaml_true_string)
+    _is_parsable_yaml, generated_data = is_parsable_yaml(yaml_pred_string)
+    is_area_exact_match = ResultDataType.FALSE
+    are_entities_exactly_same = ResultDataType.FALSE
+    are_relations_exactly_same = ResultDataType.NOT_APPLICABLE
+    are_properties_exactly_same = ResultDataType.NOT_APPLICABLE
 
-    if data2:
-        if compare_areas(data1['area'], data2['area']):
-            _is_area_same = True
+    if generated_data:
+        is_area_exact_match = compare_areas(ref_data['area'], generated_data['area'])
+        are_entities_exactly_same = compare_entities(ref_data['entities'], generated_data['entities'])
 
-        if compare_entities(data1['entities'], data2['entities']):
-            are_entities_same = True
+        # todo: property check
+        predicted_entities = {gen_entity['name']: gen_entity for gen_entity in generated_data['entities']}
+        num_entities_on_ref_data = len(ref_data['entities'])
+        num_entities_on_gen_data = len(predicted_entities)
+
+        for ref_entity in ref_data['entities']:
+            if ref_entity['name'] not in predicted_entities:
+                are_properties_exactly_same = ResultDataType.FALSE
+            else:
+                if 'properties' in ref_entity:
+                    corresponding_entity_from_gen_ents = predicted_entities[ref_entity['name']]
+                    if 'properties' in corresponding_entity_from_gen_ents:
+                        are_properties_exactly_same = compare_properties(props1=ref_entity['properties'],
+                                                                         props2=corresponding_entity_from_gen_ents[
+                                                                             'properties'])
 
         # todo: recheck this!!
-        if 'relations' not in data2:
-            are_relations_same = False
+        if 'relations' not in ref_data:
+            are_relations_exactly_same = ResultDataType.NOT_APPLICABLE
 
         else:
-            if 'relations' not in data1:
-                are_relations_same = False
+            if 'relations' not in generated_data:
+                are_relations_exactly_same = ResultDataType.FALSE
+
             else:
-                if compare_relations(data1['relations'], data2['relations']):
-                    are_relations_same = True
+                are_relations_exactly_same = compare_relations(ref_data['relations'], generated_data['relations'])
 
     return Result(yaml_pred_string=yaml_pred_string,
                   yaml_true_string=yaml_true_string,
                   is_parsable_yaml=_is_parsable_yaml,
-                  is_area_same=_is_area_same,
-                  are_entities_same=are_entities_same,
-                  are_relations_same=are_relations_same)
+                  is_area_exact_match=is_area_exact_match,
+                  num_entities_on_ref_data=num_entities_on_ref_data,
+                  num_entities_on_gen_data=num_entities_on_gen_data,
+                  are_entities_exactly_same=are_entities_exactly_same,
+                  are_relations_exactly_same=are_relations_exactly_same,
+                  are_properties_exactly_same=are_properties_exactly_same)
 
 
 if __name__ == '__main__':
@@ -199,7 +236,6 @@ if __name__ == '__main__':
     gold_sheet_name = args.gold_sheet_name
     gold_labels = pd.read_excel(gold_file_path, sheet_name=gold_sheet_name).to_dict(orient='records')
 
-
     results = []
     for prediction, gold_label in tqdm(zip(predictions, gold_labels), total=len(gold_labels)):
         assert prediction['sentence'] == gold_label['sentence']
@@ -209,9 +245,14 @@ if __name__ == '__main__':
         results.append(result.dict())
 
     results = pd.DataFrame(results)
-    for result_type in ['is_parsable_yaml','is_area_same', 'are_entities_same', 'are_relations_same']:
-        true_preds = results[results[result_type]]
-        acc = len(true_preds) / len(results)
+    for result_type in ['is_parsable_yaml', 'is_area_exact_match', 'are_entities_exactly_same',
+                        'are_relations_exactly_same',
+                        'are_properties_exactly_same']:
+        print(f"===Results for {result_type}===")
+        na_samples = results[results[result_type] == ResultDataType.NOT_APPLICABLE]
+        print(f"Number of NA samples: {len(na_samples)}")
+        true_preds = results[results[result_type] == ResultDataType.TRUE]
+        acc = len(true_preds) / (len(results) - len(na_samples))
         print(f'Accuracy of {result_type}')
         print(acc)
 
