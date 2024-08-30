7b639a3bd92c67e2e6072375a4b3e9b7e4cba0fa
Ipek Baris Schlicht <ipek.baris-schlicht@dw.com>
2024-08-30 09:10:05 +0200

    add new benchmarking

--- scripts/run_benchmarking.sh
+++ scripts/run_benchmarking.sh
@@ -58,17 +58,32 @@
 #--out_file_path $OUT_FILE_PATH \
 #--out_file_path_sum $OUT_FILE_PATH_SUM
 
-echo llama3 v15 Results
+#echo llama3 v15 Results
+#
+#GOLD_FILE_PATH=benchmarking/data/gold_annotations_18082024.xlsx
+#PRED_FILE_PATH=benchmarking/results/spot_llama3_training_ds_v15.jsonl
+#GOLD_SHEET_NAME=gold_annotations_18082024
+#OUT_FILE_PATH=benchmarking/results/spot_llama3_training_ds_v15_18082024_eval.xlsx
+#OUT_FILE_PATH_SUM=benchmarking/results/spot_llama3_training_ds_v15_8082024_eval-summary.xlsx
+#
+#python -m benchmarking.evaluate_results \
+#--gold_file_path $GOLD_FILE_PATH \
+#--pred_file_path $PRED_FILE_PATH \
+#--gold_sheet_name $GOLD_SHEET_NAME \
+#--out_file_path $OUT_FILE_PATH \
+#--out_file_path_sum $OUT_FILE_PATH_SUM
 
-GOLD_FILE_PATH=benchmarking/data/gold_annotations_18082024.xlsx
-PRED_FILE_PATH=benchmarking/results/spot_llama3_training_ds_v15.jsonl
-GOLD_SHEET_NAME=gold_annotations_18082024
-OUT_FILE_PATH=benchmarking/results/spot_llama3_training_ds_v15_18082024_eval.xlsx
-OUT_FILE_PATH_SUM=benchmarking/results/spot_llama3_training_ds_v15_8082024_eval-summary.xlsx
-
-python -m benchmarking.evaluate_results \
---gold_file_path $GOLD_FILE_PATH \
---pred_file_path $PRED_FILE_PATH \
---gold_sheet_name $GOLD_SHEET_NAME \
---out_file_path $OUT_FILE_PATH \
---out_file_path_sum $OUT_FILE_PATH_SUM
+#echo llama3 v16.1 Results
+#
+#GOLD_FILE_PATH=benchmarking/data/gold_annotations_18082024.xlsx
+#PRED_FILE_PATH=benchmarking/results/spot_llama3_training_ds_v16_1.jsonl
+#GOLD_SHEET_NAME=gold_annotations_18082024
+#OUT_FILE_PATH=benchmarking/results/spot_llama3_training_ds_v16_1_18082024_eval.xlsx
+#OUT_FILE_PATH_SUM=benchmarking/results/spot_llama3_training_ds_v16_1_8082024_eval-summary.xlsx
+#
+#python -m benchmarking.evaluate_results \
+#--gold_file_path $GOLD_FILE_PATH \
+#--pred_file_path $PRED_FILE_PATH \
+#--gold_sheet_name $GOLD_SHEET_NAME \
+#--out_file_path $OUT_FILE_PATH \
+#--out_file_path_sum $OUT_FILE_PATH_SUM
