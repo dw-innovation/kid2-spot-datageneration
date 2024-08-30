507c877cc1c5fa9ac78935248ba3dd7411714579
Ipek Baris Schlicht <ipek.baris-schlicht@dw.com>
2024-08-30 13:42:25 +0200

    creation of 16v2

--- scripts/generate_samples_with_gpt.sh
+++ scripts/generate_samples_with_gpt.sh
@@ -36,16 +36,16 @@ VERSION=v16
 
 python -m datageneration.gpt_data_generator \
 --relative_spatial_terms_path datageneration/data/relative_spatial_terms.csv \
---tag_query_file datageneration/results/${VERSION}/samples_case_props_v2.jsonl \
---output_prompt_generations datageneration/results/${VERSION}/prompt_generations_case_props_v2.jsonl \
---output_gpt_generations datageneration/results/${VERSION}/gpt_generations_case_props_v2.jsonl \
+--tag_query_file datageneration/results/${VERSION}/samples_case_props_v3.jsonl \
+--output_prompt_generations datageneration/results/${VERSION}/prompt_generations_case_props_v3.jsonl \
+--output_gpt_generations datageneration/results/${VERSION}/gpt_generations_case_props_v3.jsonl \
 --persona_path datageneration/prompts/personas.txt \
 --styles_path datageneration/prompts/styles.txt \
---prob_usage_of_relative_spatial_terms 0.7 \
+--prob_usage_of_relative_spatial_terms 0.9 \
 --prob_usage_of_written_numbers 0.25 \
 --prob_distance_writing_no_whitespace 0.4 \
---prob_distance_writing_with_full_metric 0.5 \
---prob_of_typos 0.5 \
+--prob_distance_writing_with_full_metric 0.3 \
+--prob_of_typos 0.8 \
 --max_dist_digits 5 \
 --save_yaml_csv \
 --generate_prompts \
