07e8d23a0c3a03ef8544bbd46b355e32114cc043
Ipek Baris Schlicht <ipek.baris-schlicht@dw.com>
2024-09-05 16:19:55 +0200

    update script

--- scripts/run_benchmarking.sh
+++ scripts/run_benchmarking.sh
@@ -87,3 +87,18 @@
 #--gold_sheet_name $GOLD_SHEET_NAME \
 #--out_file_path $OUT_FILE_PATH \
 #--out_file_path_sum $OUT_FILE_PATH_SUM
+
+echo llama3 v16.2 Results
+
+GOLD_FILE_PATH=benchmarking/data/gold_annotations_18082024.xlsx
+PRED_FILE_PATH=benchmarking/results/spot_llama3_training_ds_v16_2.jsonl
+GOLD_SHEET_NAME=gold_annotations_18082024
+OUT_FILE_PATH=benchmarking/results/spot_llama3_training_ds_v16_2_18082024_eval.xlsx
+OUT_FILE_PATH_SUM=benchmarking/results/spot_llama3_training_ds_v16_2_8082024_eval-summary.xlsx
+
+python -m benchmarking.evaluate_results \
+--gold_file_path $GOLD_FILE_PATH \
+--pred_file_path $PRED_FILE_PATH \
+--gold_sheet_name $GOLD_SHEET_NAME \
+--out_file_path $OUT_FILE_PATH \
+--out_file_path_sum $OUT_FILE_PATH_SUM
