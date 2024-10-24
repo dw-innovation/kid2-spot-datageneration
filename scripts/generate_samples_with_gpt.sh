aae447ed8398cf7ea435545b597b2d8597465e79
Ipek Baris Schlicht <ipek.baris-schlicht@dw.com>
2024-10-24 10:54:14 +0200

    color imp

--- scripts/generate_samples_with_gpt.sh
+++ scripts/generate_samples_with_gpt.sh
@@ -52,11 +52,28 @@ VERSION=v17
 #--generate_sentences
 
 
+#python -m datageneration.gpt_data_generator \
+#--relative_spatial_terms_path datageneration/data/relative_spatial_terms.csv \
+#--tag_query_file datageneration/results/${VERSION}/samples_case_contain_cuisine.jsonl \
+#--output_prompt_generations datageneration/results/${VERSION}/prompts_case_contain_cuisine.jsonl \
+#--output_gpt_generations datageneration/results/${VERSION}/gpt_generations_case_contain_cuisine.jsonl \
+#--persona_path datageneration/prompts/personas.txt \
+#--styles_path datageneration/prompts/styles.txt \
+#--prob_usage_of_relative_spatial_terms 0.9 \
+#--prob_usage_of_written_numbers 0.25 \
+#--prob_distance_writing_no_whitespace 0.4 \
+#--prob_distance_writing_with_full_metric 0.3 \
+#--prob_of_typos 0.4 \
+#--max_dist_digits 5 \
+#--save_yaml_csv \
+#--generate_prompts \
+#--generate_sentences
+
 python -m datageneration.gpt_data_generator \
 --relative_spatial_terms_path datageneration/data/relative_spatial_terms.csv \
---tag_query_file datageneration/results/${VERSION}/samples_case_contain_cuisine.jsonl \
---output_prompt_generations datageneration/results/${VERSION}/prompts_case_contain_cuisine.jsonl \
---output_gpt_generations datageneration/results/${VERSION}/gpt_generations_case_contain_cuisine.jsonl \
+--tag_query_file datageneration/results/${VERSION}/samples_case_contain_colors.jsonl \
+--output_prompt_generations datageneration/results/${VERSION}/prompts_case_contain_colors.jsonl \
+--output_gpt_generations datageneration/results/${VERSION}/gpt_generations_case_contain_colors.jsonl \
 --persona_path datageneration/prompts/personas.txt \
 --styles_path datageneration/prompts/styles.txt \
 --prob_usage_of_relative_spatial_terms 0.9 \
