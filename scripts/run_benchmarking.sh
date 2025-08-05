e6118f600a93d0cf7125ebfa392b2c0c6db63443
Ipek Baris Schlicht <ipek.baris-schlicht@dw.com>
2025-08-05 16:44:07 +0200

    added remote inference

--- scripts/run_benchmarking.sh
+++ scripts/run_benchmarking.sh
@@ -268,11 +268,27 @@
 #--out_file_path $OUT_FILE_PATH \
 #--out_file_path_sum $OUT_FILE_PATH_SUM
 
-FNAME=spot_Mistral-Small-24B-Base-2501-unsloth_ep10_training_ds_v16_3-17_1_2-18_3_param-4_prompt-v2-benchmarking050625
+#FNAME=spot_Mistral-Small-24B-Base-2501-unsloth_ep10_training_ds_v16_3-17_1_2-18_3_param-4_prompt-v2-benchmarking050625
+#KEY_TABLE_PATH=datageneration/data/Spot_primary_keys_bundles.xlsx
+#GOLD_FILE_PATH=benchmarking/data/gold_annotations_05062025.xlsx
+#PRED_FILE_PATH=benchmarking/results/${FNAME}.jsonl
+#GOLD_SHEET_NAME=gold_annotations_05062025
+#OUT_FILE_PATH=benchmarking/results/${FNAME}_eval.xlsx
+#OUT_FILE_PATH_SUM=benchmarking/results/${FNAME}_06062025_eval-summary.xlsx
+#
+#python -m benchmarking.evaluate_results  \
+#--key_table_path $KEY_TABLE_PATH \
+#--gold_file_path $GOLD_FILE_PATH \
+#--pred_file_path $PRED_FILE_PATH \
+#--gold_sheet_name $GOLD_SHEET_NAME \
+#--out_file_path $OUT_FILE_PATH \
+#--out_file_path_sum $OUT_FILE_PATH_SUM
+
+FNAME=spot_Mistral-Small-24B-Base-2501-unsloth_ep10_training_ds_v16_3-17_1_2-18_3_param-4_prompt-v2-benchmarking050625_remote
 KEY_TABLE_PATH=datageneration/data/Spot_primary_keys_bundles.xlsx
-GOLD_FILE_PATH=benchmarking/data/gold_annotations_05062025.xlsx
+GOLD_FILE_PATH=benchmarking/data/gold_annotations_14072025.xlsx
 PRED_FILE_PATH=benchmarking/results/${FNAME}.jsonl
-GOLD_SHEET_NAME=gold_annotations_05062025
+GOLD_SHEET_NAME=gold_annotations_14072025
 OUT_FILE_PATH=benchmarking/results/${FNAME}_eval.xlsx
 OUT_FILE_PATH_SUM=benchmarking/results/${FNAME}_06062025_eval-summary.xlsx
 
