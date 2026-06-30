1808f132d33078d315d0a389dce638aa86c7a25a
BarisSchlichtI <barisschlichti@bnlx1082682.dwelle.de>
2026-06-30 14:41:38 +0200

    update data

---
+++ scripts/run_api_predictions.sh
@@ -0,0 +1,11 @@
+FNAME=gpt_oss_remote
+GOLD_SHEET_NAME=descriptor_updates_13052026
+BENCHMARKING_FILE=benchmarking/data/goldstandard_testing_dataset.xlsx
+OUTPUT_FILE=benchmarking/results/${FNAME}.jsonl
+ENVIRONMENT=development
+
+python -m benchmarking.run_api_predictions \
+--output_file $OUTPUT_FILE \
+--benchmarking_file $BENCHMARKING_FILE \
+--gold_sheet_name $GOLD_SHEET_NAME \
+--environment $ENVIRONMENT
